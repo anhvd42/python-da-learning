b7291e3a0401633a072b832892133a6d05ec8c1a
Van Duc Anh <anhvd42@gmail.com>
2025-08-11 20:37:57 +0700

    add file Lesson 2

---
+++ Bai2. Comment/03.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+# Nhập dữ liệu
+a = input ("Nhap vào A: ")
+
+if a > 0 :
+    print("A la so duong.")
+    
+# Xử lý
+b = int(a) + 2
+
+# Xuất dữ liệu
+print(b)
