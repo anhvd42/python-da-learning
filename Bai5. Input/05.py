bfc688815ea52eab2f74ac9b87699382b33cb51a
Van Duc Anh <anhvd42@gmail.com>
2025-08-11 21:31:03 +0700

    Complete lesson 5 - Input

---
+++ Bai5. Input/05.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 11 21:22:39 2025
+
+@author: vandu
+"""
+
+input("Nhập vào 1 con số: ")
+
+a = input("Nhap vào a: ")
+print("a = {0}".format(a))
+
+ho = input("Nhập vào họ: ")
+ten = input("Nhap vào tên: ")
+
+print("Xin chào",ho,ten)
