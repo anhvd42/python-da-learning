608faf1abc1f632994bba45d72c03de62e4a9cf4
Van Duc Anh <anhvd42@gmail.com>
2025-08-11 21:04:55 +0700

    Upload lesson 4- Print

---
+++ Bai3. Print/04.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 11 20:44:01 2025
+
+@author: vandu
+Nội dung: Video số 4. Giới thiệu câu lệnh Print
+
+"""
+
+#Các loại print
+print()
+print(5)
+print("hello world!")
+print(6); print("Hi")
+print("xin","chào","Duc Anh")
+print("xin","chào","Duc Anh","1234",sep=",")
+print('Xin',"chào", end=": ")
+print("Đức Anh")
+print("Tên ={0},Họ ={1}".format("Ducanh",'Van'))
