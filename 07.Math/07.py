f6065887c78830627c58919a08e69a08deae8259
Van Duc Anh <anhvd42@gmail.com>
2025-08-18 21:45:39 +0700

    Complete lesson 9

---
+++ 07.Math/07.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 18 20:13:17 2025
+
+@author: vandu
+"""
+
+a = int(input ("Nhap vào số a : "))
+
+b = int(input("Nhập vào số b : "))
+
+print ("{0} + {1} = {2}".format(a, b  ,int(a)+int(b)))
+print ("{0} - {1} = {2}".format(a, b  ,int(a)-int(b)))
+print ("{0} * {1} = {2}".format(a, b  ,int(a)*int(b)))
+print ("{0} / {1} = {2}".format(a, b  ,int(a)/int(b)))
+print ("{0} % {1} = {2}".format(a, b  ,int(a)%int(b)))
+print ("{0} // {1} = {2}".format(a, b ,int(a) // int(b)))
+print ("{0} ** {1} = {2}".format(a, b ,int(a)**int(b)))
